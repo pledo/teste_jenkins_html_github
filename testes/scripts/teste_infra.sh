308daf9da80c699bba364f7d1c9fb1717437f06a
Paulo Ledo <paulofledo@gmail.com>
2015-01-12 17:22:02 -0200

    teste pos build

--- testes/scripts/teste_infra.sh
+++ testes/scripts/teste_infra.sh
@@ -2,8 +2,13 @@
 
 function testando_servico()
 {
-	result=$(/etc/init.d/apache2 status > /dev/null;echo $?)
-	#result=`/etc/init.d/apache2 status > /dev/null;echo $?`
+#ToDo:
+# Parametrizar o  seviço e a porta para
+# Generalizar a funcao
+# Escrever em Python
+
+#	result=$(/etc/init.d/apache2 status > /dev/null;echo $?)
+	result=$(/usr/sbin/service apache2 status > /dev/null;echo $?)
 	if [ ${result} == 0 ];then
 		echo "O resultado do comando e:${result}, servico no ar" 
 	elif [ ${result} == 3 ];then
