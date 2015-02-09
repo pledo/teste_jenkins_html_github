d0e40006e014efa658708a3911ec95969fd82962
Paulo Ledo <paulofledo@gmail.com>
2015-02-09 19:11:03 -0200

    Organizando o conteudo que importa para o deployment pipeline em um direotior para poder apontar um link simbolico para o lastStable build e facilitar a configuração da build para o CD

--- conteudo/testes/infra/teste_infra.sh
+++ conteudo/testes/infra/teste_infra.sh
@@ -15,13 +15,13 @@ function testando_servico()
 
 	result=$(/usr/sbin/service apache2 status > /dev/null;echo $?)
 	if [ ${result} == 0 ];then
-		echo "O resultado do comando e:${result}, servico no ar" 
+		#echo "O resultado do comando e:${result}, servico no ar" 
 	  return 0
   elif [ ${result} == 3 ];then
-		echo "O resultado do comando e:${result}, servico fora do ar" 
+		#echo "O resultado do comando e:${result}, servico fora do ar" 
 	  return 2
   else	
-		echo "Servico com problemas"
+		#echo "Servico com problemas"
 	  return 1
 	fi
 }
---
+++ conteudo/testes/infra/teste_posdeploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+url="127.0.0.1"
+#string_requerida="testando o deploy do html"
+string_requerida="It works"
+function pos_build()
+{
+	#result=$(curl -sG 10.200.0.82|egrep -q "testando o deploy do html";echo $?)
+	result=$(curl -sG ${url}|egrep "${string_requerida}" > /dev/null;echo $?)
+	if [ ${result} == 0 ];then
+#		echo "Testes passaram"
+		return 0
+	else	
+#		echo "Testes falharam"
+		return 2
+	fi
+}
+pos_build
