5be3038557a1ebfe7d91a866268949450748691d
Paulo Ledo <paulofledo@gmail.com>
2015-02-09 19:28:34 -0200

    Inserindo a verbosidade nos scripts de teste, he bom prq sai no console deo jenkins, assim da para saber onde esta quebrando soh olhando esse log

--- conteudo/testes/infra/teste_posdeploy.sh
+++ conteudo/testes/infra/teste_posdeploy.sh
@@ -8,10 +8,10 @@ function pos_build()
 	#result=$(curl -sG 10.200.0.82|egrep -q "testando o deploy do html";echo $?)
 	result=$(curl -sG ${url}|egrep "${string_requerida}" > /dev/null;echo $?)
 	if [ ${result} == 0 ];then
-#		echo "Testes passaram"
+		echo "Testes passaram"
 		return 0
 	else	
-#		echo "Testes falharam"
+		echo "Testes falharam"
 		return 2
 	fi
 }
