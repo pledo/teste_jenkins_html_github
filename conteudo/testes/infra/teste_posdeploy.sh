d412353348d9da5b4026d30a2b838d630254e95b
Paulo Ledo <paulofledo@gmail.com>
2015-02-11 14:25:35 -0200

    Inserindo no log qual a string requerida

--- conteudo/testes/infra/teste_posdeploy.sh
+++ conteudo/testes/infra/teste_posdeploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 url="127.0.0.1"
-#string_requerida="testando o deploy do html"
-string_requerida="It works"
+string_requerida="testando o deploy do html"
+#string_requerida="It works"
 function pos_build()
 {
-	#result=$(curl -sG 10.200.0.82|egrep -q "testando o deploy do html";echo $?)
+	echo "A string requerida he:$string_requerida"
+  #result=$(curl -sG 10.200.0.82|egrep -q "testando o deploy do html";echo $?)
 	result=$(curl -sG ${url}|egrep "${string_requerida}" > /dev/null;echo $?)
 	if [ ${result} == 0 ];then
 		echo "Testes passaram"
